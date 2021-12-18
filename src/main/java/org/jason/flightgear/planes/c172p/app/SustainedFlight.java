55834b160bc40bf2d1a523c3ede6ab545429babd
jas0ndiamond <jason.a.diamond@gmail.com>
2021-12-18 12:45:47 -0800

    long overdue cleanup. more logging. terminate simulator at end of flight. refuel individual tanks. more field in telemetry readout

--- src/main/java/org/jason/flightgear/planes/c172p/app/SustainedFlight.java
+++ src/main/java/org/jason/flightgear/planes/c172p/app/SustainedFlight.java
@@ -2,6 +2,7 @@ package org.jason.flightgear.planes.c172p.app;
 
 import java.io.IOException;
 
+import org.apache.commons.net.telnet.InvalidTelnetOptionException;
 import org.jason.flightgear.exceptions.FlightGearSetupException;
 import org.jason.flightgear.flight.util.FlightUtilities;
 import org.jason.flightgear.planes.c172p.C172P;
@@ -10,16 +11,18 @@ import org.slf4j.LoggerFactory;
 
 public class SustainedFlight {
 		
-	private static Logger logger = LoggerFactory.getLogger(SustainedFlight.class);
+	private static Logger LOGGER = LoggerFactory.getLogger(SustainedFlight.class);
 	
-	//private final static int EXPECTED_TRACK = 30;
 	private final static int TARGET_ALTITUDE = 9000;
 	
 	//0 => N, 90 => E
-	private final static int TARGET_HEADING = 180;
+	private final static int TARGET_HEADING = 90;
 	
 	private static void launch(C172P plane) throws IOException {
 		//assume start unpaused;
+		
+		//assume already set
+		double takeoffHeading = plane.getHeading();
 		
 		plane.setPause(true);
 		
@@ -32,15 +35,15 @@ public class SustainedFlight {
 		plane.setPause(false);
 		
 		int i = 0;
-		while( i < 50) {
-			FlightUtilities.airSpeedCheck(plane, 10, 90);
+		while( i < 20) {
+			//FlightUtilities.airSpeedCheck(plane, 10, 100);
 			
 			FlightUtilities.altitudeCheck(plane, 500, TARGET_ALTITUDE);
 			FlightUtilities.pitchCheck(plane, 4, 3.0);
 			FlightUtilities.rollCheck(plane, 4, 0.0);
 			
 			//narrow heading check on launch
-			FlightUtilities.headingCheck(plane, 4, TARGET_HEADING);
+			FlightUtilities.headingCheck(plane, 4, takeoffHeading);
 			
 			i++;
 			try {
@@ -50,6 +53,18 @@ public class SustainedFlight {
 			}
 		}
 		
+		//set while not paused. this functions more like a boost- 
+		//the plane can be acceled or deceled to the specified speed, 
+		//but then the fdm takes over and stabilizes the air speed
+//		plane.setAirSpeed(100);
+//		
+//		//initial drop. allow to level off
+//		try {
+//			Thread.sleep(40*1000);
+//		} catch (InterruptedException e) {
+//			e.printStackTrace();
+//		}
+		
 		//////
 		//initial check that we've leveled off
 		FlightUtilities.altitudeCheck(plane, 500, TARGET_ALTITUDE);
@@ -58,25 +73,29 @@ public class SustainedFlight {
 		
 		//increase throttle
 		plane.setPause(true);
-		plane.setThrottle(0.80);
+		plane.setThrottle(0.95);
 		plane.setPause(false);
 	}
 	
 	private static String telemetryReadOut(C172P plane) {
 				
 		return 
-			String.format("\nHeading: %f", plane.getHeading()) +
-			String.format("\nAir Speed: %f", plane.getAirSpeed()) +
-			String.format("\nThrottle: %f", plane.getThrottle()) +
-			String.format("\nMixture: %f", plane.getMixture()) +
-			String.format("\nFuel level: %f", plane.getFuelLevel()) +
-			String.format("\nEngine running: %d", plane.getEngineRunning()) + 
-			String.format("\nAltitude: %f", plane.getAltitude()) +
-			String.format("\nLatitude: %f", plane.getLatitude()) + 
-			String.format("\nLongitude: %f", plane.getLongitude());
+				String.format("\nCurrent Heading: %f", plane.getHeading()) +
+				String.format("\nAir Speed: %f", plane.getAirSpeed()) +
+				String.format("\nFuel tank 0 level: %f", plane.getFuelTank0Level()) +
+				String.format("\nFuel tank 1 level: %f", plane.getFuelTank1Level()) +
+				String.format("\nBattery level: %f", plane.getBatteryCharge()) +
+				String.format("\nEngine running: %d", plane.getEngineRunning()) + 
+				String.format("\nEngine rpms: %f", plane.getEngineRpms()) + 
+				String.format("\nEnv Temp: %f", plane.getTemperature()) + 
+				String.format("\nThrottle: %f", plane.getThrottle()) +
+				String.format("\nMixture: %f", plane.getMixture()) +
+				String.format("\nAltitude: %f", plane.getAltitude()) +
+				String.format("\nLatitude: %f", plane.getLatitude()) + 
+				String.format("\nLongitude: %f", plane.getLongitude());
 	}
 	
-	public static void main(String [] args) throws IOException {
+	public static void main(String [] args) {
 		C172P plane = null;
 		
 		try {
@@ -88,18 +107,14 @@ public class SustainedFlight {
 			plane.refillFuel();
 			
 			try {
-				Thread.sleep(10000);
+				Thread.sleep(5000);
 			} catch (InterruptedException e1) {
 				e1.printStackTrace();
 			}
 			
 			double currentHeading = (TARGET_HEADING);
 			
-			//head north
 			plane.setHeading(currentHeading);
-			
-			//TODO: check if engine running, plane is in the air, speed is not zero
-			//
 			
 			plane.startupPlane();
 	
@@ -117,7 +132,7 @@ public class SustainedFlight {
 			int cycles = 0;
 			int maxCycles = 50* 1000;
 				
-			int minFuelGal = 4;
+			int minFuelGal = 16;
 			
 			plane.setBatterySwitch(false);
 			
@@ -133,50 +148,69 @@ public class SustainedFlight {
 			
 			while(running && cycles < maxCycles) {
 				
-				logger.info("======================\nCycle {} start. Target heading: {} ", cycles, currentHeading);
+				LOGGER.info("======================\nCycle {} start. Target heading: {} ", cycles, currentHeading);
 				
 				//check altitude first, if we're in a nose dive that needs to be corrected first
 				FlightUtilities.altitudeCheck(plane, 500, TARGET_ALTITUDE);
 				
 				//TODO: ground elevation check. it's a problem if your target alt is 5000ft and you're facing a 5000ft mountain
 				
-				FlightUtilities.pitchCheck(plane, 4, 2.0);
-				
-				FlightUtilities.rollCheck(plane, 4, 0.0);
-				
-				//check heading last-ish, correct pitch/roll first otherwise the plane will probably drift off heading quickly
-				FlightUtilities.headingCheck(plane, 8, currentHeading);
-				
-				//FlightUtilities.airSpeedCheck(plane, 10, 90);
-				
-				//check fuel last last. easy to refuel
-				if(plane.getFuelLevel() < minFuelGal) {
-					plane.refillFuel();
+				if(cycles % 50 == 0 ) {
+					plane.forceStabilize(currentHeading, 0, 2.0);
+				} else {
+					FlightUtilities.pitchCheck(plane, 4, 2.0);
+
+					FlightUtilities.rollCheck(plane, 4, 0.0);
+
+					// check heading last-ish, correct pitch/roll first otherwise the plane will
+					// probably drift off heading quickly
+					
+					FlightUtilities.headingCheck(plane, 4, currentHeading);
 				}
 				
-				logger.info("Telemetry Read: {}", telemetryReadOut(plane));
+				//check fuel last last. easy to refuel
+				//refill both tanks for balance
+				if (plane.getFuelTank0Level() < minFuelGal || plane.getFuelTank1Level() < minFuelGal) {
+					plane.refillFuelTank0();
+					plane.refillFuelTank1();
+				}
+				
+				LOGGER.info("Telemetry Read: {}", telemetryReadOut(plane));
 				
 				try {
 					Thread.sleep(cycleSleep);
 				} catch (InterruptedException e) {
 					e.printStackTrace();
 				}
-				
-				if(cycles % 100 == 0) {
-					currentHeading += 30;
-					currentHeading %= 360;
-				}
+
+				//optionally change direction slightly
+//				if(cycles % 100 == 0) {
+//					currentHeading += 30;
+//					currentHeading %= 360;
+//				}
 				
 				cycles++;
 				
-				logger.info("Cycle end\n======================");
+				LOGGER.info("Cycle end\n======================");
 			}
+			
+			LOGGER.info("Trip is finished!");
 		} catch (FlightGearSetupException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		finally {
 			if(plane != null) {
 				plane.shutdown();
+			}
+			
+			try {
+				plane.terminateSimulator();
+			} catch (IOException e) {
+				e.printStackTrace();
+			} catch (InvalidTelnetOptionException e) {
+				e.printStackTrace();
 			}
 		}
 	}
