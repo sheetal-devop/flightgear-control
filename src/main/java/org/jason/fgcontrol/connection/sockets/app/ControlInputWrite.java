75026370e291c8ea5241d201775aa6d6fe2e1d90
jas0ndiamond <jason.a.diamond@gmail.com>
2023-05-13 16:29:07 -0700

    Testing partial updates of simulator control input

--- src/main/java/org/jason/fgcontrol/connection/sockets/app/ControlInputWrite.java
+++ src/main/java/org/jason/fgcontrol/connection/sockets/app/ControlInputWrite.java
@@ -1,21 +1,54 @@
 package org.jason.fgcontrol.connection.sockets.app;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.PrintWriter;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
-import java.net.Socket;
-import java.net.UnknownHostException;
 import java.nio.charset.Charset;
 
+/**
+ * A test of updating fields in a custom-defined input protocol.
+ *
+ */
 public class ControlInputWrite {
 
+	public static void performWrite(String host, int port, String payload) {
+        
+        System.out.println("FG Control input: " + payload);
+        
+        DatagramSocket fgInputSocket = null;
+        
+        try {
+	        byte[] fgInputPayload = payload.getBytes(Charset.forName("UTF-8"));
+	        
+	        DatagramPacket fgInputPacket = new DatagramPacket(
+	                fgInputPayload, 
+	                fgInputPayload.length, 
+	                InetAddress.getByName(host), 
+	                port
+	        );
+	        
+	        fgInputPacket.setData(payload.getBytes(Charset.forName("UTF-8")));
+	        
+	        fgInputSocket = new DatagramSocket();
+	        
+	        fgInputSocket.setSoTimeout(5000);
+	        
+	        fgInputSocket.send(fgInputPacket);
+        }
+        catch(Exception e) {
+        	e.printStackTrace();
+        } 
+        finally {
+        	if(fgInputSocket != null) {
+        		fgInputSocket.close();
+        	}
+        }
+	}
+	
     public static void main(String[] args) {
         
         if(args.length != 2) {
-        	System.err.println("Usage: TelemetryReadControlInputWrite [host] [inputPort]");
+        	System.err.println("Usage: ControlInputWrite [host] [inputPort]");
         	System.exit(-1);
         }
         
@@ -32,101 +65,41 @@ public class ControlInputWrite {
         		System.exit(-1);
         	}
         }
-        
-        Socket fgConnection = null; 
-        PrintWriter output = null;
-        BufferedReader input = null;
-                       
-        try {            
-            DatagramSocket fgInputSocket = null;
-            DatagramPacket fgInputPacket = null;
-            
-            for(int i = 0; i< 5; i++) {
-                
-                //test input//////////////
-                //read on next loop iteration
-                
-                //fgInputSocket = new DatagramSocket(FG_SOCKETS_INPUT_PORT, InetAddress.getByName(FG_SOCKETS_HOST) );
-                
-                /*	f15c orientation typically on port 5227 for F15C Beta
-                 
-                     <!-- orientation -->
-			         <chunk>
-			            <node>/orientation/heading-deg</node>
-			            <name>heading-deg</name>
-			            <type>float</type>
-			         </chunk>
-			         <chunk>
-			            <node>/orientation/pitch-deg</node>
-			            <name>pitch-deg</name>
-			            <type>float</type>
-			         </chunk>
-			         <chunk>
-			            <node>/orientation/roll-deg</node>
-			            <name>roll-deg</name>
-			            <type>float</type>
-			         </chunk>
-                 */
-            	//rotate the plane in 15 degree chunks clockwise
-                String fgInput = "\n" + ((double)i * 15.0) % 360.0 + "," + 0.0 + "," + 0.0 +"\n";
-                
-                System.out.println("FG Control input: " + fgInput);
-                
-                byte[] fgInputPayload = fgInput.getBytes(Charset.forName("UTF-8"));
-                
-                fgInputPacket = new DatagramPacket(
-                        fgInputPayload, 
-                        fgInputPayload.length, 
-                        InetAddress.getByName(host), 
-                        inputPort
-                );
-                
-                fgInputPacket.setData(fgInput.getBytes(Charset.forName("UTF-8")));
-                
-                fgInputSocket = new DatagramSocket();
-                
-                fgInputSocket.setSoTimeout(5000);
-                
-                fgInputSocket.send(fgInputPacket);
 
-                
-                
-                fgInputSocket.close();
-                
-                try {
-                    Thread.sleep(3000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        } 
-        catch (UnknownHostException e) {
-            e.printStackTrace();
-        } 
-        catch (IOException e) {
-            e.printStackTrace();
-        }
-        finally {
-            
-            if(output != null) {
-                output.close();
-            }
-            
-            if(input != null) {
-                try {
-                    input.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            
-            if(fgConnection != null) {
-                try {
-                    fgConnection.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
+		for (int i = 0; i < 5; i++) {
+
+			// test input//////////////
+			// read on next loop iteration
+
+			// fgInputSocket = new DatagramSocket(FG_SOCKETS_INPUT_PORT,
+			// InetAddress.getByName(FG_SOCKETS_HOST) );
+
+			// rotate the plane in 15 degree chunks clockwise
+			String fgInput = "\n" + ((double) i * 15.0) % 360.0 + "," + 0.0 + "," + 0.0 + "\n";
+
+			System.out.println("FG Control input: " + fgInput);
+
+			ControlInputWrite.performWrite(host, inputPort, fgInput);
+		}
     }
+    
+    /*	f15c orientation typically on port 5227 for F15C Beta
+    
+    <!-- orientation -->
+    <chunk>
+       <node>/orientation/heading-deg</node>
+       <name>heading-deg</name>
+       <type>float</type>
+    </chunk>
+    <chunk>
+       <node>/orientation/pitch-deg</node>
+       <name>pitch-deg</name>
+       <type>float</type>
+    </chunk>
+    <chunk>
+       <node>/orientation/roll-deg</node>
+       <name>roll-deg</name>
+       <type>float</type>
+    </chunk>
+*/
 }
