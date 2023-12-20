71327a46f72bb2dd9487d2b5470bf2600eb2fdca
jas0ndiamond <jason.a.diamond@gmail.com>
2023-12-20 11:29:28 -0800

    built out user store loading. not quite working

--- src/main/java/org/jason/fgcontrol/gateway/app/poc/BasicAuthWebSecurityConfiguration.java
+++ src/main/java/org/jason/fgcontrol/gateway/app/poc/BasicAuthWebSecurityConfiguration.java
@@ -1,7 +1,13 @@
 package org.jason.fgcontrol.gateway.app.poc;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Map.Entry;
+import java.util.Properties;
 
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,7 +28,8 @@ public class BasicAuthWebSecurityConfiguration {
 
 	private final static Logger LOGGER = LoggerFactory.getLogger(BasicAuthWebSecurityConfiguration.class);
 	
-	private final static String USER_STORE = "users.properties";
+	// TODO: change to distribution location
+	private final static String USER_STORE = "src/main/resources/users.properties";
 	private final static String GATEWAY_USER_ROLE = "USER_ROLE";
 	
 	// TODO: migrate to reading hashes from file
@@ -44,7 +51,8 @@ public class BasicAuthWebSecurityConfiguration {
 			.disable()
 			.authorizeRequests()
 			.antMatchers(
-				"/helloworld/*"
+				"/helloworld/*",
+				"/actuator/shutdown"
 			)
 			.permitAll()
 			.anyRequest()
@@ -58,28 +66,118 @@ public class BasicAuthWebSecurityConfiguration {
 		return http.build();
 	}
 
+	/**
+	 * Load the users from the user store. Works with hardcoded user but not ones loaded from store
+	 * 
+	 * TODO: ^^^^ fix
+	 * 
+	 * @return
+	 * @throws Exception
+	 */
 	@Bean
-	public InMemoryUserDetailsManager userDetailsService() {
+	public InMemoryUserDetailsManager userDetailsService() throws Exception {
 
 		LOGGER.debug("Gateway userDetailsService invoked");
 		
 		//TODO: load users from userstore
 
-		ArrayList<UserDetails> users = new ArrayList<>();
+		ArrayList<UserDetails> users = null;
 
-		UserDetails user = 
-			User.withUsername(TEST_USER)
-				.password(
-					passwordEncoder().encode(TEST_PASS)
-				)
-				.roles(GATEWAY_USER_ROLE)
-				.build();
+		InMemoryUserDetailsManager userDetailsManager = null;
+		
+		Properties userlist = new Properties();
+		try {
+			userlist.load(new InputStreamReader(new FileInputStream(USER_STORE)));
+			
+			users = new ArrayList<>();
+			
+			//userlist should be a list of users mapped to hash strings
+			
+			String iUsername, iUserAuthHash, iUsernameCheck;
+			for( Entry<Object, Object> userEntry : userlist.entrySet()) {
+				//TODO: load into list
+				iUsername = String.valueOf(userEntry.getKey());
+				
+				if(iUsername == null || StringUtils.isEmpty(iUsername)) {
+					LOGGER.warn("Failed to read username from store");
+				} else {
+				
+					LOGGER.debug("Loading user {}", iUsername);
+					
+					iUserAuthHash = String.valueOf(userEntry.getValue());
+					if(iUserAuthHash == null || StringUtils.isEmpty(iUserAuthHash)) {
+						LOGGER.warn("Failed to read user auth hash from store");
+					}
+					else {
+						
+						//require full hash string: fgctl:$2y$08$derphash
+						
+						int hashDelimIndex = iUserAuthHash.indexOf(':');
+						
+						iUsernameCheck = iUserAuthHash.substring(0, hashDelimIndex);
+						
+						if(iUsername.equals(iUsernameCheck)) {
+							iUserAuthHash = iUserAuthHash.substring(hashDelimIndex + 1);
 
-		users.add(user);
+							//LOGGER.debug("Read user hash {}", iUserAuthHash);
 
+							users.add(
+								User.withUsername(iUsername)
+									.password( iUserAuthHash )
+									.roles(GATEWAY_USER_ROLE)
+									.build()
+							);
+							
+							iUserAuthHash = null;
+							
+							LOGGER.debug("Loaded user {} successfully", iUsername);
+						} else {
+							LOGGER.error("User check failed");
+						}
+					}
+				}
+			}
+			
+			// hardcoded users	
+			
+			UserDetails user = 
+				User.withUsername(TEST_USER)
+					.password(
+						passwordEncoder().encode(TEST_PASS)
+					)
+					.roles(GATEWAY_USER_ROLE)
+					.build();
+
+			users.add(user);
+			
+			int loadedUserCount = users.size();
+			
+			LOGGER.debug("Loaded {} users from user store", loadedUserCount);
+			
+			if(loadedUserCount == 0) {
+				throw new Exception("No users loaded");
+			}
+			
+			userDetailsManager = new InMemoryUserDetailsManager(users);
+			
+		} catch (IOException e) {
+			LOGGER.error("IOException loading userlist", e);
+			throw e;
+		} catch (Exception e) {
+			LOGGER.error("Exception loading userlist", e);
+			throw e;
+		}
+		finally {
+			if(userDetailsManager == null) {
+				LOGGER.error("Failed to load users from user store");	
+			} else {
+				LOGGER.info("Loaded users from user store");
+			}
+		}
+		
 		LOGGER.debug("Gateway userDetailsService returning");
 		
-		return new InMemoryUserDetailsManager(users);
+		return userDetailsManager;
 	}
 
 	@Bean
